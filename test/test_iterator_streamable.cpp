684927c3ce8207f7ba76316d9b895c672430e3f6
KitsuneAlex <alexh@karmakrafts.dev>
2023-03-27 13:26:13 +0200

    Implement unit tests for IteratorStreamable

---
+++ test/test_iterator_streamable.cpp
@@ -0,0 +1,36 @@
+// Copyright 2023 Karma Krafts & associates
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @author Alexander Hinze
+ * @since 27/03/2023
+ */
+
+#include <vector>
+#include <gtest/gtest.h>
+#include <cxxstreams/iterator_streamable.hpp>
+
+TEST(cxxstreams_IteratorStreamable, TestIterate) {
+    std::vector<float> values({1.0F, 2.0F, 3.0F, 4.0F, 1.5F, 2.5F, 3.5F, 4.5F});
+    auto streamable = cxxstreams::IteratorStreamable(values.cbegin(), values.cend());
+
+    for(const auto value : values) {
+        auto element = streamable.next();
+        ASSERT_TRUE(element);
+        ASSERT_EQ(*element, value);
+    }
+
+    auto element = streamable.next();
+    ASSERT_FALSE(element);
+}
